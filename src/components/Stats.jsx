f323091b86590fd32894e19e2f00694b7fbf7a9a
Ayumi Saito <ayumisaito@Ayumis-MacBook-Air.local>
2017-09-07 11:51:37 +0200

    stat.page flexbox

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
+function Analytics(prop){
+    return(
+     <div className="analytics">
+    <h2>ANALYTICS</h2>
+    <ul>
+    <li> New Users</li>
+    <li> Total Users</li>
+    <li> Total Conversations</li>
+    <li> Total Messages Sent</li>
+    <li> Total Messages Received</li>
+    <li> Handover Requests</li>
+ </ul>
+  </div>
+    )
+}
+
+function Fallback(prop){
+  return(
+  <div className="fallback">
+<h2>Fallback - NonConfigured</h2>
+<ul>
+<li> Total Handover Requests</li>
+<li> Agents Available</li>
+<li> Agents Unavailable</li>
+<li> API Calls</li>
+ </ul>
+  </div>
+  )
+}
+
+
 export default class Stats extends React.Component {
-
-    constructor(props) {
-    super(props);
-    }
-
+    
     render() {
-    return (<div> Stats go here </div>);
+    return (
+    <div> 
+    <Analytics />
+    <Fallback />
+    </div>);
     }
 }
