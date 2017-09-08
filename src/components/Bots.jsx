49b96aa6126a290e5f24c59a7c0cb1e187aadea1
Ayumi Saito <ayumisaito@Ayumis-MacBook-Air.local>
2017-09-08 15:21:37 +0200

    test commit

--- src/components/Bots.jsx
+++ src/components/Bots.jsx
@@ -17,8 +17,10 @@ constructor(props){
 render(){
  const categoryStatus = this.state.isOpen ? "isopen" : "";
   return(
+  <div>
+  <h3>CATEGORIES</h3>
+  <button onClick={this.handleClick}>Click</button>
   <div className={categoryStatus} id="category">
-  <div><h3>CATEGORIES</h3></div>
   <input className="categoryInput" type="text" value="Add Category" />
   <ul>
   <li>Greetings</li>
@@ -26,28 +28,12 @@ render(){
   <li>Interjections</li>
   </ul>
   </div>
+  </div>
   )}
 }
 
 class Accessory extends React.Component {
-render() {
-  return(
-  <div className="botAccessory">
-  <div className="firstContainer">
-  <navbar><ul>
-  {/* to be added */}
-  <li id="one"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
-  <li id="two"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
-  <li id="three"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
-  </ul></navbar>
-  </div>
-  <div className="secondContainer"></div>
-  </div>
-  )}
-}
-
-class CreateBotForm extends React.Component {
-     constructor(props){
+constructor(props){
      super(props);
      this.state={
       isOpen: false
@@ -57,13 +43,45 @@ class CreateBotForm extends React.Component {
 
     handleClick(){
     this.setState({ isOpen : !this.state.isOpen})
+    console.log(!this.state.isOpen)
   }
-  
-    render() {
-    const botStatus = this.state.isOpen ? "isopen" : "";
-    const categoryStatus = this.state.isOpen ? "isopen" : "";
+render() {
+const accessoryStatus = this.state.isOpen ? "isopen" : "";
+  return(
+  <div>
+  <button onClick={this.handleClick}>Click</button>
+  <div className={accessoryStatus} id="accessoryStatus">
+  <div className="firstContainer">
+  <navbar><ul>
+  <li id="one"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
+  <li id="two"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
+  <li id="three"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
+  </ul></navbar>
+  </div>
+  <div className="secondContainer"></div>
+  </div>
+  </div>
+  )}
+}
+
+class CreateBotForm extends React.Component {
+    constructor(props){
+     super(props);
+     this.state={
+      isOpen: false
+      }
+     this.handleClick = this.handleClick.bind(this)
+     }
+
+    handleClick(){
+    this.setState({ isOpen : !this.state.isOpen})
+    console.log(!this.state.isOpen)
+  }
+    render() { 
+    const botStatus = !this.state.isOpen ? "isopen" : "";
     return (
-    <div className={botStatus} id="botForm">
+    <div className="botStatus" id="botStatus">
+    <button onClick={this.handleClick}>Click</button>
     <h2>MASTER INTENTS</h2>
     <input type="text" value="Add Intent" />
     </div>);
@@ -103,12 +121,12 @@ export default class Bots extends React.Component {
 
     render() {
     const chatbots = this.state.chatbots.map((bot) => <Chatbot bot={bot} key={bot.id}/>);
-    const botStatus = this.state.isOpen ? "isopen" : "";
+    const categoryStatus = this.state.isOpen ? "isopen" : "";
     return (<div>
         <div className="botPage">
        
-        <Category />
-        <CreateBotForm className={botStatus} tenants={this.state.tenants} addChatbot={this.addBot} onClick={this.handleClick} />
+        <Category className={categoryStatus} onClick={this.handleClick} />
+        <CreateBotForm className={categoryStatus} tenants={this.state.tenants} addChatbot={this.addBot} />
         <Accessory />
         </div>
         </div>);
