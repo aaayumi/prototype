81afcc96955b3d15de1702b933258ced950b33ef
Ayumi Saito <ayumisaito@Ayumis-MacBook-Air.local>
2017-09-12 16:17:11 +0200

    data completed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -9,18 +9,18 @@ const newUserData = {
       label: 'New Users',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(177, 58, 201,0.4)',
+      borderColor: 'rgba(177, 58, 201,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(109, 28, 125,1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 10,
+      pointHoverBackgroundColor: 'rgba(177, 58, 201,1)',
+      pointHoverBorderColor: 'rgba(109, 28, 125,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
@@ -33,25 +33,25 @@ const totalUser = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
   datasets: [
     {
-      label: 'New Users',
+      label: 'Total User',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(177, 58, 201,0.4)',
+      borderColor: 'rgba(177, 58, 201,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(109, 28, 125,1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 10,
+      pointHoverBackgroundColor: 'rgba(177, 58, 201,1)',
+      pointHoverBorderColor: 'rgba(109, 28, 125,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+      data: [300,340,400,450,500,560,600,650,700,750,800,900]
     }
   ]
 };
@@ -60,25 +60,25 @@ const totalConversation = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
   datasets: [
     {
-      label: 'New Users',
+      label: 'Total Conversation',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(177, 58, 201,0.4)',
+      borderColor: 'rgba(177, 58, 201,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(109, 28, 125,1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 10,
+      pointHoverBackgroundColor: 'rgba(177, 58, 201,1)',
+      pointHoverBorderColor: 'rgba(109, 28, 125,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+      data: [30,60,65,70,80,90,95,100,103,105,110,112]
     }
   ]
 };
@@ -87,25 +87,25 @@ const totalMessage = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
   datasets: [
     {
-      label: 'New Users',
+      label: 'Total Message Sent',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(177, 58, 201,0.4)',
+      borderColor: 'rgba(177, 58, 201,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(109, 28, 125,1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 10,
+      pointHoverBackgroundColor: 'rgba(177, 58, 201,1)',
+      pointHoverBorderColor: 'rgba(109, 28, 125,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+      data: [50,70,90,100,120,130,140,160,186,200,210,300]
     }
   ]
 };
@@ -114,25 +114,25 @@ const totalMessageReceived = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
   datasets: [
     {
-      label: 'New Users',
+      label: 'Total Message Received',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(177, 58, 201,0.4)',
+      borderColor: 'rgba(177, 58, 201,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(109, 28, 125,1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 10,
+      pointHoverBackgroundColor: 'rgba(177, 58, 201,1)',
+      pointHoverBorderColor: 'rgba(109, 28, 125,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+      data: [102,100,130,134,110,100,111,123,120,125,130,123]
     }
   ]
 };
@@ -146,14 +146,14 @@ const handover= {
   datasets: [{
     data: [300, 50, 100],
     backgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    '#FFCE56'
+    '#B13AC9',
+    '#33B86F',
+    '#F4EB32'
     ],
     hoverBackgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    '#FFCE56'
+    '#B13AC9',
+    '#33B86F',
+    '#F4EB32'
     ]
   }]
 };
@@ -199,10 +199,10 @@ function Fallback(prop){
   return( <div className="fallback">
   <h2 className="fallbackTitle">Fallback - NonConfigured</h2>
   <ul>
-      <li> Total Handover Requests</li>
-      <li> Agents Available</li>
-      <li> Agents Unavailable</li>
-      <li> API Calls</li>
+      <li> Total Handover Requests  0</li>
+      <li> Agents Available 0</li>
+      <li> Agents Unavailable 10</li>
+      <li> API Calls 10</li>
   </ul>
   </div>
   )
