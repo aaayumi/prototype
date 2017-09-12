5cd415b832a9abc2ccbbeb65a0f6e54651c69798
Ayumi Saito <ayumisaito@Ayumis-MacBook-Air.local>
2017-09-12 15:49:10 +0200

    data added

--- src/components/Bots.jsx
+++ src/components/Bots.jsx
@@ -51,48 +51,44 @@ export default class Bots extends React.Component {
         <div className="botPage">
         <div>
 
-   <div className="categoryContainer">
-   <div className={categoryContainer}>
+        <div className="categoryContainer">
+        <div className={categoryContainer}>
         <h3>CATEGORIES</h3>
- </div>
- <button onClick={this.handleClick}>
-
-  {this.state.caOpen
-    ? <i className="slideinButton fa fa-share fa-2x" aria-hidden="true"></i>
-    : <i className="slideinButton fa fa-reply fa-2x" aria-hidden="true"></i>
-  }
-
- </button>
-  
- </div>
-  <div className={categoryStatus} id="category">
-  <input className="categoryInput" type="text" value="Add Category" placeholder="Add Category" />
-  <ul>
-      <li>Greetings</li>
-      <li>Main Switchboard</li>
-      <li>Interjections</li>
+        </div>
+        <button onClick={this.handleClick}>
+       {this.state.caOpen
+       ? <i className="slideinButton fa fa-share fa-2x" aria-hidden="true"></i>
+       : <i className="slideinButton fa fa-reply fa-2x"  aria-hidden="true"></i>
+       }
+       </button>
+       </div>
+      
+       <div className={categoryStatus} id="category">
+       <input className="categoryInput" type="text" value="Add Category" placeholder="Add Category" />
+      <ul>
+          <li>Greetings</li>
+          <li>Main Switchboard</li>
+          <li>Interjections</li>
       </ul>
-  </div>
-  </div>
+      </div>
+      </div>
 
        <div>
-   
-    <div className={botStatus} id="bot">
-        <h2>MASTER INTENTS</h2>
-        <input id="botInput" className={botInput} type="text" value="Add Intent" />
-      
-    </div>
-    </div>
+       <div className={botStatus} id="bot">
+       <h2>MASTER INTENTS</h2>
+      <input id="botInput" className={botInput} type="text" value="Add Intent" />
+       </div>
+       </div>
 
-        <div className="accessoryContainer">
-  <div className={accessoryButton} id="accessoryButton">
+      <div className="accessoryContainer">
+      <div className={accessoryButton} id="accessoryButton">
       <button onClick={this.clickHandle}>{this.state.acOpen
     ? <i className="slideoutButton fa fa-reply fa-2x" aria-hidden="true"></i>
     : <i className="slideoutButton fa fa-share fa-2x" aria-hidden="true"></i>
   }</button>
-  </div>
-  <div className={accessoryStatus} id="accessory">
-  <div className="firstContainer">
+      </div>
+      <div className={accessoryStatus} id="accessory">
+      <div className="firstContainer">
       <navbar>
       <ul>
           <li id="one"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
@@ -100,12 +96,12 @@ export default class Bots extends React.Component {
           <li id="three"><i className="fa fa-bolt fa-2x" aria-hidden="true"></i></li>
       </ul>
       </navbar>
-  </div>
-  <div className="secondContainer">
-  </div>
-  </div>
-  </div>
-        </div>
-        </div>);
+      </div>
+      <div className="secondContainer">
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>);
     }
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,19 +1,199 @@
 import React from 'react';
+import {Line} from 'react-chartjs-2';
+import {Pie} from 'react-chartjs-2';
 
-function Analytics(prop){
-    return(<div className="analytics">
+const newUserData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+  datasets: [
+    {
+      label: 'New Users',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+    }
+  ]
+};
+
+const totalUser = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+  datasets: [
+    {
+      label: 'New Users',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+    }
+  ]
+};
+
+const totalConversation = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+  datasets: [
+    {
+      label: 'New Users',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+    }
+  ]
+};
+
+const totalMessage = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+  datasets: [
+    {
+      label: 'New Users',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+    }
+  ]
+};
+
+const totalMessageReceived = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+  datasets: [
+    {
+      label: 'New Users',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [30,40,45,50,55,50,60,60,56,58,61,55]
+    }
+  ]
+};
+
+const handover= {
+  labels: [
+    'Question completed',
+    'Question incompleted',
+    'Handover to human'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+    '#FF6384',
+    '#36A2EB',
+    '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+    '#FF6384',
+    '#36A2EB',
+    '#FFCE56'
+    ]
+  }]
+};
+
+
+class Analytics extends React.Component {
+  constructor(props){
+  super(props);
+  this.state ={
+  width: 200,
+  height: 100
+  }
+  }
+
+  render(){
+  return(<div className="analytics">
     <h2 className="analyticsTitle">ANALYTICS</h2>
      <ul>
-        <li> New Users</li>
-        <li> Total Users</li>
-        <li> Total Conversations</li>
-        <li> Total Messages Sent</li>
-        <li> Total Messages Received</li>
-        <li> Handover Requests</li>
+        <li> <div className="newData">
+    <Line id="newUserData" data ={newUserData} height={this.state.height} width={this.state.width} />
+    </div></li>
+        <li> <div className="newData">
+    <Line id="newUserData" data ={totalUser} height={this.state.height} width={this.state.width} />
+    </div></li>
+        <li><div className="newData">
+    <Line id="newUserData" data ={totalConversation} height={this.state.height} width={this.state.width} />
+    </div></li>
+        <li> <div className="newData">
+    <Line id="newUserData" data ={totalMessage} height={this.state.height} width={this.state.width} />
+    </div></li>
+        <li> <div className="newData">
+    <Line id="newUserData" data ={totalMessageReceived} height={this.state.height} width={this.state.width} />
+    </div></li>
+        <li>   <Pie data={handover} /></li>
      </ul>
           </div>
     )
+  }
 }
+
 
 function Fallback(prop){
   return( <div className="fallback">
